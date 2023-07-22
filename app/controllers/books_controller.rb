2406ef6c3ceac285ed42e1c337283fb0108e33a4
EC2 Default User <ec2-user@ip-172-31-11-38.ap-northeast-1.compute.internal>
2023-07-22 06:37:32 +0000

    [add]左側完

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,17 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(post_image_params)
+    @book.user_id = current_user.id
+    if @book.save
+    redirect_to book_path
+    else
+      render :'users#index'
+    end
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @profile_image = @user.profile_image
+    @book = Book.new
   end
 
   def edit
