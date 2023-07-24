c1cc8191d2ea5dff0ffe3378f403b033c87dacf0
EC2 Default User <ec2-user@ip-172-31-11-38.ap-northeast-1.compute.internal>
2023-07-24 11:49:12 +0000

    [add]rspec修正中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,40 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
   
   def create
-    @books = Book.new(book_params)
-    @books.user_id = current_user.id
-    if @books.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have create book successfully"
+      redirect_to book_path(@book.id)
     else
-      render :'books/show'
+      @books = Book.all
+      render :index
     end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def index
     @users = User.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
@@ -10,5 +13,40 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You have create user successfully"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+  
 end
